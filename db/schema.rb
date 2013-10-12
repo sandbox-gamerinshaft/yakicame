5a50e2dd2656ac28dcd43ac50c30a783f44f28a3
j.tane <taneichijunpei@gamerinshaft.local>
2013-10-12 21:42:41 +0900

    migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131012121338) do
+ActiveRecord::Schema.define(:version => 20131012123956) do
+
+  create_table "homes", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "tweets", :force => true do |t|
     t.integer  "user_id"
