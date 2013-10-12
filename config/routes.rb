d4df60702d6d2c4aadcccaff939689815717ddf5
j.tane <taneichijunpei@gamerinshaft.local>
2013-10-12 21:24:30 +0900

    generate tweets controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Yakicame1::Application.routes.draw do
+  get "tweets/index"
+
+  get "tweets/new"
+
+  get "tweets/create"
+
+  get "tweets/show"
+
+  get "tweets/edit"
+
+  get "tweets/update"
+
+  get "tweets/destroy"
+
   devise_for :users
 
   # The priority is based upon order of creation:
