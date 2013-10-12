d79217f9d28ef8075c3e4d66a815ba864ba7a78b
j.tane <taneichijunpei@gamerinshaft.local>
2013-10-12 21:04:16 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yakicame1::Application.config.secret_token = '577d23e5d3d5c0b1a92aa2f620a21c2303ca99a03aa2c754044331257c0126e6322570d0a3e9a9edf8c6c95ac32ab9523adf1315ee0ddafb7f1e3679becccdf5'
