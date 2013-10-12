4de2e9d277e80fa3842c08e5d47811ac8ddec7e9
j.tane <taneichijunpei@gamerinshaft.local>
2013-10-12 22:01:14 +0900

    setting link sign in sign up

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Yakicame1::Application.routes.draw do
   resources :tweets
   devise_for :users
 
+  unauthenticated do
+    as :user do
+      root to: 'homes#index'
+    end
+  end  
   root to: 'tweets#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
