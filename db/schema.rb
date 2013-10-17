1fab9865a83f19f7c604b2994a34503875215826
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-10-17 13:36:41 +0900

    generate devise:views

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131012123956) do
+ActiveRecord::Schema.define(:version => 20131013131248) do
 
   create_table "homes", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(:version => 20131012123956) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
