132eabd65601f98a9c84e57caeb2636d823d1d2a
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-10-21 11:22:13 +0900

    フォロー前のあらかたの基本的機能を設定し終わった。後々細かくつくる

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,9 +2,9 @@ class Tweet < ActiveRecord::Base
   attr_accessible :content,:timage
   belongs_to :user
   mount_uploader :timage, ImageUploader
-  default_scope order("created_at DESC").includes(:favorites)
+  default_scope order("created_at DESC").includes(:favorites,:user)
   has_many :favorites,dependent: :destroy
-
+  
   def favorited? user
     favorites.any? do |f|
       f.user == user
