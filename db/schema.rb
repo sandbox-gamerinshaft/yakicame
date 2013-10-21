36051816e618a822cad6e427e89009c3b954b07d
taneichijunpei@gamerinshaft <j.tane3ma@gmail.com>
2013-10-21 11:24:06 +0900

    migration database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131020054612) do
+ActiveRecord::Schema.define(:version => 20131021022326) do
 
   create_table "favolites", :force => true do |t|
     t.integer  "user_id"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(:version => 20131020054612) do
     t.string   "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "follows", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "homes", :force => true do |t|
